a7c8297976f6578dfae41f3f558a8b68970066bb
naonao-program <gf19a012@oecu.jp>
2020-12-19 18:11:44 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,13 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
   
   validates :content, presence: true
+end
+
+private
+
+def message_params
+  params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
 end
